e0aab9894543f53b928feb19f5e836dc8a171d9f
Jaymie Xu <jaymie.xsj@gmail.com>
2019-04-25 14:05:21 -0400

    Remove point based on simplex

--- Assets/Scripts/GJK/GJK.cs
+++ Assets/Scripts/GJK/GJK.cs
@@ -78,67 +78,88 @@ public class GJK
 
             // Add 4th point
             state.CurrentSimplex.Push(SupportFunction(state.LastDirection, state.GetPolytopeA, state.GetPolytopeB));
-
-            // Debug info
-            Vector3 pointD = state.CurrentSimplex.PeekAt(3); // Newest
-
-            Vector3 vecDC = pointC - pointD;
-            Vector3 vecDB = pointB - pointD;
-            Vector3 vecDA = pointA - pointD;
-
-            Vector3 normDCB = Vector3.Cross(vecDC, vecDB);
-            Vector3 normDCA = Vector3.Cross(vecDC, vecDA);
-            Vector3 normDBA = Vector3.Cross(vecDA, vecDB);
-
-            state.MiscDebugLines.Add(normDCB);
-            state.MiscDebugLines.Add(normDCA);
-            state.MiscDebugLines.Add(normDBA);
         }
         // Tetrahedron
         else
         {
-            // Clear lines
-            state.MiscDebugLines.Clear();
+            Vector3 origin = Vector3.zero;
 
-            Vector3 pointA = state.CurrentSimplex.PeekAt(0); // Oldest
-            Vector3 pointB = state.CurrentSimplex.PeekAt(1);
-            Vector3 pointC = state.CurrentSimplex.PeekAt(2);
-            Vector3 pointD = state.CurrentSimplex.PeekAt(3); // Newest
+            Vector3 point0 = state.CurrentSimplex.PeekAt(0);
+            Vector3 point1 = state.CurrentSimplex.PeekAt(1);
+            Vector3 point2 = state.CurrentSimplex.PeekAt(2);
+            Vector3 point3 = state.CurrentSimplex.PeekAt(3);
 
-            Vector3 vecDC = pointC - pointD;
-            Vector3 vecDB = pointB - pointD;
-            Vector3 vecDA = pointA - pointD;
+            Matrix4x4 d0 = new Matrix4x4();
+            Matrix4x4 d1 = new Matrix4x4();
+            Matrix4x4 d2 = new Matrix4x4();
+            Matrix4x4 d3 = new Matrix4x4();
+            Matrix4x4 d4 = new Matrix4x4();
 
-            Vector3 normDCB = Vector3.Cross(vecDC, vecDB);
-            Vector3 normDCA = Vector3.Cross(vecDC, vecDA);
-            Vector3 normDBA = Vector3.Cross(vecDA, vecDB);
+            d0.SetRow(0, new Vector4(point0.x, point0.y, point0.z, 1f));
+            d0.SetRow(1, new Vector4(point1.x, point1.y, point1.z, 1f));
+            d0.SetRow(2, new Vector4(point2.x, point2.y, point2.z, 1f));
+            d0.SetRow(3, new Vector4(point3.x, point3.y, point3.z, 1f));
 
-            Vector3 directionDtoO = new Vector3(0f, 0f, 0f) - pointD;
+            d1.SetRow(0, new Vector4(origin.x, origin.y, origin.z, 1f));
+            d1.SetRow(1, new Vector4(point1.x, point1.y, point1.z, 1f));
+            d1.SetRow(2, new Vector4(point2.x, point2.y, point2.z, 1f));
+            d1.SetRow(3, new Vector4(point3.x, point3.y, point3.z, 1f));
 
-            if (normDCB.IsInSameDirection(Vector3.zero))
+            d2.SetRow(0, new Vector4(point0.x, point0.y, point0.z, 1f));
+            d2.SetRow(1, new Vector4(origin.x, origin.y, origin.z, 1f));
+            d2.SetRow(2, new Vector4(point2.x, point2.y, point2.z, 1f));
+            d2.SetRow(3, new Vector4(point3.x, point3.y, point3.z, 1f));
+
+            d3.SetRow(0, new Vector4(point0.x, point0.y, point0.z, 1f));
+            d3.SetRow(1, new Vector4(point1.x, point1.y, point1.z, 1f));
+            d3.SetRow(2, new Vector4(origin.x, origin.y, origin.z, 1f));
+            d3.SetRow(3, new Vector4(point3.x, point3.y, point3.z, 1f));
+
+            d4.SetRow(0, new Vector4(point0.x, point0.y, point0.z, 1f));
+            d4.SetRow(1, new Vector4(point1.x, point1.y, point1.z, 1f));
+            d4.SetRow(2, new Vector4(point2.x, point2.y, point2.z, 1f));
+            d4.SetRow(3, new Vector4(origin.x, origin.y, origin.z, 1f));
+
+            // Determinants
+            float det0 = d0.determinant;
+            float det1 = d1.determinant;
+            float det2 = d2.determinant;
+            float det3 = d3.determinant;
+            float det4 = d4.determinant;
+
+            // Degenerate simplex :(
+            if (det0 == 0f)
             {
-                // Remove oldest(A aka index 0)
-                state.CurrentSimplex.RemoveAt(0);
-                state.LastDirection = normDCB;
+                state.CurrentSimplex.PopBack();
             }
-            else if (normDCA.IsInSameDirection(Vector3.zero))
+            // Check if all five det have same sign
+            else if (Mathf.Sign(det0) == Mathf.Sign(det1)
+                && Mathf.Sign(det1) == Mathf.Sign(det2)
+                && Mathf.Sign(det2) == Mathf.Sign(det3)
+                && Mathf.Sign(det3) == Mathf.Sign(det4))
             {
-                // Remove second oldest(B aka index 1)
-                state.CurrentSimplex.RemoveAt(1);
-                state.LastDirection = normDCA;
-            }
-            else if (normDBA.IsInSameDirection(Vector3.zero))
-            {
-                // Remove third oldest(C aka index 2)
-                state.CurrentSimplex.RemoveAt(2);
-                state.LastDirection = normDBA;
-            }
-            else
-            {
-                Debug.Log("Finished a GJK IT");
-
-                // Origin inside
                 state.FinishRun = true;
+                Debug.Log("Collided.");
+            }
+            // Drop point 0
+            else if (Mathf.Sign(det0) != Mathf.Sign(det1))
+            {
+                state.CurrentSimplex.RemoveAt(0);
+            }
+            // Drop point 1
+            else if (Mathf.Sign(det0) != Mathf.Sign(det2))
+            {
+                state.CurrentSimplex.RemoveAt(1);
+            }
+            // Drop point 2
+            else if (Mathf.Sign(det0) != Mathf.Sign(det3))
+            {
+                state.CurrentSimplex.RemoveAt(2);
+            }
+            // Drop point 3
+            else if (Mathf.Sign(det0) != Mathf.Sign(det4))
+            {
+                state.CurrentSimplex.RemoveAt(3);
             }
         }
     }
